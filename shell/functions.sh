dfd7f3e94b8398c6b0a5a75be83306e9cca07e67
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2020-12-27 17:00:05 -0500

    fix spacing

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,22 +1,22 @@
 # Makes starting a tmux session convenient
 st(){
 if [ "$TMUX" = "" ]; then
-    # check for old session
-    if [ "$(tmux ls | grep -v attached | wc -l)" -gt "0" ]; then
-        # attach to old session
-        tmux a -t "$(tmux ls | grep -v attached | cut -d ":" -f1 | head -n 1)"
-    else
-        # start new session - dont use exec so it's possible to run without tmux
-        tmux
-    fi
+  # check for old session
+  if [ "$(tmux ls | grep -v attached | wc -l)" -gt "0" ]; then
+      # attach to old session
+      tmux a -t "$(tmux ls | grep -v attached | cut -d ":" -f1 | head -n 1)"
+  else
+      # start new session - dont use exec so it's possible to run without tmux
+      tmux
+  fi
 fi
 }
 
 # DotFiles Update
 dfu() {
-    (
-        cd ~/.dotfiles; git pull --ff-only; make install; cd -
-    )
+  (
+      cd ~/.dotfiles; git pull --ff-only; make install; cd -
+  )
 }
 
 # Run to get load time for shell
