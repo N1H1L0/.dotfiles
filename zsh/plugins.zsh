3ce6a00a880d056491500b92365e0fab28ec6349
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2020-12-31 09:55:53 -0500

    disable autoupdate

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -9,10 +9,11 @@ fi
 END
 
 # Oh My Zsh settings
-
 export ZSH="$HOME/.dotfiles/zsh/plugins/ohmyzsh"
 
 ZSH_THEME="cypher"
+
+DISABLE_AUTO_UPDATE="true"
 
 # If plugins are taking to long to load you could switch to using Zgen load some on startup and then load the rest with a function if you don't need every plugin at startup
 plugins=(git vi-mode pip brew osx asdf)
