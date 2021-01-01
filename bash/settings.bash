3fd1740ee5929ef1f13b8b45d1bdb4b989254c81
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2021-01-01 15:28:11 -0500

    asdf bash upgrade

--- bash/settings.bash
+++ bash/settings.bash
@@ -14,25 +14,16 @@ export HISTCONTROL=ignoreboth
 export HISTSIZE=5000
 export HISTIGNORE="clear:bg:fg:cd:cd -:cd ..:exit:date:w:* --help:ls:l:ll:lll"
 
-# Detect the platform (similar to $OSTYPE)
-OS="`uname`"
-case $OS in
-  'Linux')
-    # echo 'Linux'
-
-    # asdf installed with git 
-    . $HOME/.asdf/asdf.sh
-    . $HOME/.asdf/completions/asdf.bash
-    ;;
-  'Darwin')
-    # echo 'macOS'
-
-    # asdf installed with brew
-    . /usr/local/opt/asdf/asdf.sh
-    . /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
-    ;;
-  *)
-    # echo 'other os'
-    ;;
-esac
+if [ -d "$HOME/.asdf" ]; then 
+  # asdf installed with git 
+  . $HOME/.asdf/asdf.sh
+  . $HOME/.asdf/completions/asdf.bash
+elif [ "brew ls --versions asdf >/dev/null" ]; then 
+  # asdf installed with brew
+  . /usr/local/opt/asdf/asdf.sh
+  . /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
+else 
+  # do nothing so bash doesn't give errors with asdf isn't installed
+  :
+fi
 
