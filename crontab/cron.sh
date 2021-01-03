cf88966c3bab637229f9408aa7baed063ad84ea4
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2021-01-03 12:27:18 -0500

    remove .DS_Store cron job

--- crontab/cron.sh
+++ crontab/cron.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+!/bin/bash
 
 if ! crontab -l | grep "$HOME/dotfiles/crontab/$(basename -- "$0")"; then
-  (crontab -l ; echo "43 00 *   *   3     find /home/$USER/.vim/undo-dir -type f -mtime +90 -delete > /dev/null 2>&1") |sort - | uniq - | crontab - 2>&1
+  (crontab -l ; echo "43 00 *   *   3     find /home/$USER/.vim/undo-dir -type f -mtime +90 -delete") |sort - | uniq - | crontab - 2>&1
+  (crontab -l ; echo "0 0 * * * root find / -name “.DS_Store” -depth -exec rm {} \;") |sort - | uniq - | crontab - 2>&1
 fi
