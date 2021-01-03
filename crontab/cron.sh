c358119075aab92659bc4c25801b0ddfd21c739c
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2021-01-03 12:37:32 -0500

    fixed cron header

--- crontab/cron.sh
+++ crontab/cron.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 if ! crontab -l | grep "$HOME/dotfiles/crontab/$(basename -- "$0")"; then
   (crontab -l ; echo "43 00 *   *   3     find /home/$USER/.vim/undo-dir -type f -mtime +90 -delete") |sort - | uniq - | crontab - 2>&1
