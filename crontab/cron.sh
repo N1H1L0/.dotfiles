f08c6869bd67b41a44b92bfd28ddbf250fb8c516
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2022-07-30 17:00:52 -0400

    simplify cron

--- crontab/cron.sh
+++ crontab/cron.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if ! crontab -l | grep "$HOME/dotfiles/crontab/$(basename -- "$0")"; then
-  (crontab -l ; echo "43 00 *   *   3     find /home/$USER/.vim/undo-dir -type f -mtime +90 -delete") |sort - | uniq - | crontab - 2>&1
-  (crontab -l ; echo "0 0 * * * root find / -name “.DS_Store” -depth -exec rm {} \;") |sort - | uniq - | crontab - 2>&1
-fi
+
+30 0 * * * root find / -name '.DS_Store' -type f -delete
